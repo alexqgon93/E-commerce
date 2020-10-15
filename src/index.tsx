6d65a12ed725eb2a604ea82e4141332efcb94f94
aeeu <alexandre.esquerdo@gft.com>
2020-10-15 16:58:14 +0200

    first set up for the new project

--- src/index.tsx
+++ src/index.tsx
@@ -3,15 +3,21 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import CustomAppBar from './components/CustomAppBar';
+import { ThemeProvider } from '@material-ui/core';
+import theme from './theme/theme';
+import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <BrowserRouter>
+        <App />
+        <CustomAppBar />
+      </BrowserRouter>
+    </ThemeProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
